221f9cf5138b252fbb3388e737a58a28646c9f55
Sagar <dev.sagartmg@gmail.com>
2024-08-07 15:21:12 +0545

    es module

---
+++ es-module/auth.js
@@ -0,0 +1,18 @@
+export const signup = () => {
+    console.log("signup")
+}
+
+export const login = () => {
+    console.log("login")
+}
+
+// module.exports = {
+//     signup: signup,
+//     login,
+// }
+
+const checkAuthentication = () =>{
+    console.log("check authentication");
+}
+export default checkAuthentication
+
---
+++ es-module/index.js
@@ -0,0 +1,20 @@
+/* 
+    auth 
+        login
+        signup
+
+    product
+        create
+        get
+        update
+        delete
+*/
+
+// const auth = require("./auth")
+
+// auth.signup()
+// auth.login()
+
+import checkAuthentication,{ login, signup } from "./auth.js"
+login()
+signup()
